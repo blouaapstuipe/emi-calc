a684355a365a6d8cb076aa6fbe0fcd3d5e5440e3
Gilgamesh <engelkc@gmail.com>
2024-03-22 15:19:24 +0100

    components + service stubs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmiComponent } from './emi/emi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmiComponent
   ],
   imports: [
     BrowserModule,
